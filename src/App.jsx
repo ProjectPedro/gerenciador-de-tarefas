4c212772576870f2d4d90442d31badceaf711ac6
Pedro Henrique <l2ouroo@hotmail.com>
2025-10-29 15:59:38 -0300

    Adicionando Tasks com React

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import Tasks from "./Tasks.jsx";
 import AddTask from "./AddTask.jsx";
 
 function App() {
+  const[tasks,setTasks] = useState([{
+    id: 1,
+    title: "Estudar Programação",
+    isCompleted: false,
+},{
+    id: 2,
+    title: "Ler um Livro",
+    isCompleted: true,
+},{
+    id: 3,
+    title: "Fazer Exercícios",
+    isCompleted: false, 
+}])
+
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500  flex justify-center p-6">
+      <div className="w-[500px] ">
+        <h1 className="text-3xl text-slate-100 font-bold text-center"> Gerenciador de Tarefas </h1>
+        <AddTask />
+        <Tasks tasks ={tasks} />
+      </div>
     </div>
   );
 }
--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -1,5 +1,12 @@
-function Tasks() {
-    return <h1>Tasks</h1>
-}
+function Tasks(props) {
+    return (
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {props.tasks.map ((task) => ( 
+             <li key={task.id} className= "bg-slate-400 text-white p-2 rounded-md " >
+                {task.title} 
+             </li>
+          ))}
+        </ul>
+)}
 
 export default Tasks;
