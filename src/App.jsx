0f1f2c8b382a1c15a14f91dd66aeabbb0eb49346
Pedro Henrique <l2ouroo@hotmail.com>
2025-10-30 16:43:30 -0300

    States were stored, and a function was created to change the task's state.

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,23 @@ function App() {
     isCompleted: false, 
 }])
 
+function onTaskClick(taskId){
+  const newTasks = tasks.map((task) => {
+    if (taskId === task.id){
+      return {...task, isCompleted: !task.isCompleted };
+    }
+
+    return task;
+  });
+  setTasks(newTasks);
+}
+
   return (
     <div className="w-screen h-screen bg-slate-500  flex justify-center p-6">
       <div className="w-[500px] ">
         <h1 className="text-3xl text-slate-100 font-bold text-center"> Gerenciador de Tarefas </h1>
         <AddTask />
-        <Tasks tasks ={tasks} />
+        <Tasks tasks ={tasks} onTaskClick={onTaskClick}/>
       </div>
     </div>
   );
