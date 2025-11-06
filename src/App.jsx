88034cc292ae82163821d5d42a472c452d000fed
Pedro Henrique <l2ouroo@hotmail.com>
2025-11-06 15:50:34 -0300

    Test App With API

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,20 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      setTasks(data);
+    };
+    fetchInitialData();
+  }, []);
+
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       if (taskId === task.id) {
